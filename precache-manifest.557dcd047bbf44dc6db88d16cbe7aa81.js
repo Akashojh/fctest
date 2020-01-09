42d7f3b78979693c0d3eb199bb9df00018d062b9
akash <akash@emproto.com>
2020-01-09 14:47:15 +0530

    initial

---
+++ precache-manifest.557dcd047bbf44dc6db88d16cbe7aa81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a38c55eacfd6119835b0310a801cb51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7833f10330e7bf9b63e",
+    "url": "/static/css/main.bdb5db2c.chunk.css"
+  },
+  {
+    "revision": "a0afa6959daed2ea2590",
+    "url": "/static/js/2.2d40fd7e.chunk.js"
+  },
+  {
+    "revision": "0e922cdde1fe986530bb4dc0bb48f5fd",
+    "url": "/static/js/2.2d40fd7e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c7833f10330e7bf9b63e",
+    "url": "/static/js/main.f3ca65f3.chunk.js"
+  },
+  {
+    "revision": "170e518e340160b46c30",
+    "url": "/static/js/runtime-main.0a1e65f6.js"
+  },
+  {
+    "revision": "efebd8fb3eb3c9f6a774a387c05df361",
+    "url": "/static/media/logo.efebd8fb.PNG"
+  }
+]);
